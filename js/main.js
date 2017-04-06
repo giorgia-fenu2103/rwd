0294b1ba39729b78258780b05d39aad4db0d21f5
giorgia-fenu2103 <giorgiafn478@gmail.com>
2017-04-06 15:57:11 +0200

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var lorem = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio facilis, non. In quidem delectus ex illum magnam quas ea, possimus impedit quod amet doloremque deleniti nam, libero dolore eos sapiente."
+lorem = lorem + lorem + lorem + lorem + lorem + lorem + lorem + lorem + lorem + lorem + lorem;
+
+$ ('.lorem1').html(lorem);
+$ ('.lorem2').html(lorem);
+$ ('.lorem3').html(lorem);
